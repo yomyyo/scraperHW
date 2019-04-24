9cb37081e81dd7b8568cee4cb112838d1307fd30
Sammy Tang <samtang226@gmail.com>
2019-04-24 15:43:26 -0700

    heroku stuff

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
