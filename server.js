b9e5f480576d69f2c53360ea090d2cb27916647d
Sammy Tang <samtang226@gmail.com>
2019-04-24 15:38:37 -0700

    fixed issues

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Our scraping tools
@@ -19,7 +18,6 @@ var app = express();
 // Configure middleware
 
 // Use morgan logger for logging requests
-app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
